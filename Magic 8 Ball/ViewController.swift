69ddb347989c26103bf963786e4874fcc42aac8a
piowie <59792495+piowie@users.noreply.github.com>
2022-04-19 20:36:43 +0200

    Push to git

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -1,18 +1,25 @@
-//
-//  ViewController.swift
-//  Magic 8 Ball
-//
-//  Created by Angela Yu on 14/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
+    @IBOutlet weak var mainBall: UIImageView!
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+   
+    
+    let ballArray = [UIImage(imageLiteralResourceName: "ball1"),UIImage(imageLiteralResourceName: "ball2"), UIImage(imageLiteralResourceName: "ball3"), UIImage(imageLiteralResourceName: "ball4"), UIImage(imageLiteralResourceName: "ball5")]
+    
+    
+    mainBall.image = ballArray.randomElement()
+    
+    }
 
 }
+    
+    
+    
+    
+
+
+
 
